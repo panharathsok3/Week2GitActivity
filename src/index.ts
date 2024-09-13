77b74785b71d8b4261a27070c1806971b18b80f2
panharathsok3 <panharath.sok123@gmail.com>
2024-09-13 14:28:37 -0400

    added subtract cosole log

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,5 @@ const result_add = calc.add(2, 3);
 console.log(`result_add = ${result_add}`);
 
 console.log(`result_multiple = ${calc.multiply(2, 3)}`);
+console.log(`result_divide = ${calc.divide(6, 2)}`);
+console.log(`result_subtract = ${calc.subtract(6, 2)}`);
